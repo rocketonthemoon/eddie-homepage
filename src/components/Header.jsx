58544dff5aa1d9d006399ed09d536883040ea2d6
abdulsalamp <pabdulsalam@hotmail.com>
2022-12-10 14:37:00 +0200

    add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+function Header() {
+  return (
+    <header>
+      <a className="logo" href="#">
+        Edie
+      </a>
+      <nav>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Services</a>
+        </li>
+        <li>
+          <a href="#">Our Works</a>
+        </li>
+        <li>
+          <a href="#">Clients</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
